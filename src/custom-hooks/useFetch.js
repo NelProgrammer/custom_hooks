9f0e2273fd9554e1bf40850f48db50ab1a4999fa
@NelProgrammer <nelsonlzweni@gmail.com>
2024-12-31 23:11:45 +0200

    Refactored State management and useEffect to Custom Hook file - useFetch.js.
    This is to DRY and make a reusable hook that can be called in any component that needs to fetch data.
    e.g. The code was similar in App.jsx and AvailablePlaces.jsx files.

---
+++ src/custom-hooks/useFetch.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+
+const useFetch = (fetchFn, initialValue) => {
+  // fetchFn could be any function. in this case it was fetching places data from https.js
+  const [isFetching, setIsFetching] = useState();
+  const [error, setError] = useState();
+  const [fetchedData, setFetchedData] = useState(initialValue); // We don't know the innitial value upfront. It could be empty array or anything.
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsFetching(true);
+      try {
+        const data = await fetchFn();
+        setFetchedData(data);
+      } catch (error) {
+        setError({ message: error.message || 'Failed to fetch Data.' });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchData();
+  }, [fetchFn]);
+
+  return { isFetching, fetchedData, error };
+};
+
+export default useFetch;
